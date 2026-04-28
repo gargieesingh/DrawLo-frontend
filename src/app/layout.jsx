d8cf3d22455c578da43353b8b5f8c3b81386000c
Gargie <gargiesingh321@gmail.com>
2026-04-28 22:09:51 +0530

    fix(ui): fix the mobile screen auto scroll, while typing guess

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover, interactive-widget=resizes-visual" />
       </head>
       <body>
         <GameProvider>
